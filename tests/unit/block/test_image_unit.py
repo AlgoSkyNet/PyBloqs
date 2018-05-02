33138a310e74a13e9b10a7dbefb20077fcc42068
Dominik Christ <Dominik.Christ@man.com>
2018-05-02 08:23:45 +0100

    Intermediate commit

--- tests/unit/block/test_image_unit.py
+++ tests/unit/block/test_image_unit.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pytest
 from pybloqs.block.image import PlotBlock, PlotlyPlotBlock
+from mock import patch
 
 
 def test_create_PlotBlock():
@@ -29,6 +30,23 @@ def test_create_Plotly_with_invalid_data():
     with pytest.raises(ValueError):
         PlotlyPlotBlock(pd.Series([1, 2, 3]).plot())
 
+
 def test_create_Bokeh_with_invalid_data():
     with pytest.raises(ValueError):
         PlotlyPlotBlock(pd.Series([1, 2, 3]).plot())
+
+
+def test_missing_matplotlib_raises_error():
+    with patch.dict('sys.modules', {'matplotlib.pyplot': None}):
+        with pytest.raises(ImportError):
+            import pybloqs.block.image
+
+
+def test_missing_plotly_does_not_raise_error():
+    with patch.dict('sys.modules', {'plotly': None}):
+        import pybloqs.block.image
+
+
+def test_missing_bokeh_does_not_raise_error():
+    with patch.dict('sys.modules', {'bokeh': None}):
+        import pybloqs.block.image
